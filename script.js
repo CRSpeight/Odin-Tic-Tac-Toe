456322ed19c6f26519ec2e8c8ffbf2023b465027
Chris Speight <paradisio15@gmail.com>
2020-07-26 13:00:52 -0500

    Reformatted tiles and added basic event listener functinality

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const gameboard = (() => {
-  const button = "<button>BLANK</button>";
   const drawBoard = function () {
     const container = document.querySelector("#container");
-    container.innerHTML = button;
+
+    // Creates Buttons in grid
+    for (let i = 0; i < 9; i++) {
+      const button = document.createElement("button");
+      button.classList = i;
+      button.dataset.index = i;
+      button.textContent = "";
+      container.appendChild(button);
+    }
+
+    //Function executed when a button is clicked
+    function buttonClicked(e) {
+      console.log(this.dataset.index);
+      let button = document.querySelector(".\\3" + this.dataset.index);
+      button.textContent = "X";
+    }
+
+    // Adds event listeners to all buttons
+    let buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+      button.addEventListener("click", buttonClicked);
+    });
   };
   return { drawBoard };
 })();
